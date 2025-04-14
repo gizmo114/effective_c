7ad332431a3a8a9fdb5c4ca87a2fc92495cfd1b3
gizmo114 <tomasmihok114@gmail.com>
2025-04-14 21:15:45 +0200

    testing malloc and null terminating

---
+++ src/malloc_test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct User {
+  int id;
+  char name[50];
+};
+
+int main(void) {
+  struct User *user = malloc(sizeof(struct User));
+  if (user == NULL) {
+    fprintf(stderr, "Memory allocation failed!\n");
+    exit(EXIT_FAILURE);
+  }
+  user->id = 0;
+  strcpy(user->name, "John Smith");
+  printf("ID: %d, Name: %s\n", user->id, user->name);
+  char *string = malloc(10);
+  strcpy(string, "abcdefghi");
+  string[9] = '\0';
+  printf("%s\n", string);
+  return EXIT_SUCCESS;
+}
